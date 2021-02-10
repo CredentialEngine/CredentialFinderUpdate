b1f13b2989736d7ca2242fd6d81172f7bfb53aff
siuc-nate <21346519+siuc-nate@users.noreply.github.com>
2021-02-10 11:35:11 -0600

    Update TypeScriptSample.ts

--- DataModel/TypeScriptSample.ts
+++ DataModel/TypeScriptSample.ts
@@ -5,69 +5,178 @@ interface Organization {
 	CTDLType: string;
 	// Will always be a BCP-47 language code, forced to all lowercase. Usually "en". This will indicate the language used by all of the relevant string values for this record.
 	RecordLanguage: string;
-	AccreditedBy: [{Link}, ...];
-	AccreditedIn: [{JurisdictionProfile}, ...];
-	Accredits: [{Link}, ...];
-	Address: [{Place}, ...];
-	AdministrationProcess: [{ProcessProfile}, ...];
-	AgentPurpose: {Link};
+	// QAReceived (combined)
+	AccreditedBy: AJAXSettings;
+	AccreditedIn: [JurisdictionProfile, ...];
+	// QualityAssurancePerformed
+	Accredits: AJAXSettings;
+	Address: [Place, ...];
+	AdministrationProcess: [ProcessProfile, ...];
+	AgentPurpose: Link;
 	AgentPurposeDescription: string;
-	AgentSectorType: {Link};
-	AgentType: [{Link}, ...];
+	AgentSectorType: Link;
+	AgentType: [Link, ...];
 	AlternateName: [string, ...];
-	AppealProcess: [{ProcessProfile}, ...];
-	ApprovedBy: [{Link}, ...];
-	ApprovedIn: [{JurisdictionProfile}, ...];
-	Approves: [{Link}, ...];
-	AvailabilityListing: [{Link}, ...];
-	ComplaintProcess: [{ProcessProfile}, ...];
+	AppealProcess: [ProcessProfile, ...];
+	// QAReceived (combined)
+	ApprovedBy: AJAXSettings;
+	ApprovedIn: [JurisdictionProfile, ...];
+	// QualityAssurancePerformed
+	Approves: AJAXSettings;
+	AvailabilityListing: [Link, ...];
+	ComplaintProcess: [ProcessProfile, ...];
 	CTID: string;
-	Department: [{Link}, ...];
+	Department: AJAXSettings;
 	Description: string;
-	DevelopmentProcess: [{ProcessProfile}, ...];
+	DevelopmentProcess: [ProcessProfile, ...];
 	DUNS: string;
 	Email: [string, ...];
 	FEIN: string;
 	FoundingDate: string;
-	HasConditionManifest: [{ConditionManifest}, ...];
-	HasCostManifest: [{CostManifest}, ...];
-	HasVerificationService: [{ProcessProfile}, ...];
-	Identifier: [{IdentifierValue}, ...];
-	Image: {Link};
-	IndustryType: [{Link}, ...];
+	HasConditionManifest: AJAXSettings;
+	HasCostManifest: AJAXSettings;
+	HasVerificationService: [ProcessProfile, ...];
+	Identifier: [IdentifierValue, ...];
+	Image: Link;
+	IndustryType: [Link, ...];
 	IPEDSID: string;
 	ISICV4: string;
-	Jurisdiction: [{JurisdictionProfile}, ...];
+	Jurisdiction: [JurisdictionProfile, ...];
 	Keyword: [string, ...];
 	LEICode: string;
-	MaintenanceProcess: [{ProcessProfile}, ...];
-	MissionAndGoalsStatement: {Link};
+	MaintenanceProcess: [ProcessProfile, ...];
+	MissionAndGoalsStatement: Link;
 	MissionAndGoalsStatementDescription: string;
 	// Generally NAICS is merged in Industry type. So no need to display separately?
 	NAICS: [string, ...];
 	Name: string;
-	Offers: [{Link}, ...];
+	Offers: AJAXSettings;
 	OPEID: string;
-	Owns: [{Link}, ...];
-	ParentOrganization: {Link};
-	QualityAssuranceTargetType: [{Link}, ...];
-	RecognizedBy: [{Link}, ...];
-	RecognizedIn: [{JurisdictionProfile}, ...];
-	Recognizes: [{Link}, ...];
-	RegulatedBy: [{Link}, ...];
-	RegulatedIn: [{JurisdictionProfile}, ...];
-	Regulates: [{Link}, ...];
-	Renews: [{Link}, ...];
-	ReviewProcess: [{ProcessProfile}, ...];
-	RevocationProcess: [{ProcessProfile}, ...];
-	Revokes: [{Link}, ...];
-	SameAs: [{Link}, ...];
-	ServiceType: [{Link}, ...];
-	SocialMedia: [{Link}, ...];
-	SubjectWebpage: {Link};
-	SubOrganization: [{Link}, ...];
-	TransferValueStatement: {Link};
+	Owns: AJAXSettings;
+	ParentOrganization: AJAXSettings;
+	QualityAssuranceTargetType: [Link, ...];
+	// QAReceived (combined)
+	RecognizedBy: AJAXSettings;
+	RecognizedIn: [JurisdictionProfile, ...];
+	// QualityAssurancePerformed
+	Recognizes: AJAXSettings;
+	// QAReceived (combined)
+	RegulatedBy: AJAXSettings;
+	RegulatedIn: [JurisdictionProfile, ...];
+	// QualityAssurancePerformed
+	Regulates: AJAXSettings;
+	Renews: AJAXSettings;
+	ReviewProcess: [ProcessProfile, ...];
+	RevocationProcess: [ProcessProfile, ...];
+	Revokes: AJAXSettings;
+	SameAs: [Link, ...];
+	ServiceType: [Link, ...];
+	SocialMedia: [Link, ...];
+	SubjectWebpage: Link;
+	SubOrganization: AJAXSettings;
+	TransferValueStatement: Link;
 	TransferValueStatementDescription: string;
+}
+
+interface Credential {
+	// Will always be "Credential"
+	BroadType: string;
+	// Will be the Shorthand URI for one of the subclasses of ceterms:Credential (See "Credential and Subclasses": https://credreg.net/ctdl/terms#groups )
+	CTDLType: string;
+	// Will always be a BCP-47 language code, forced to all lowercase. Usually "en". This will indicate the language used by all of the relevant string values for this record.
+	RecordLanguage: string;
+	AccreditedBy: AJAXSettings;
+	AccreditedIn: [Place, ...];
+	AdministrationProcess: [ProcessProfile, ...];
+	AdvancedStandingFrom: [ConditionProfile, ...];
+	AlternateName: [string, ...];
+	AppealProcess: [ProcessProfile, ...];
+	ApprovedBy: AJAXSettings;
+	ApprovedIn: [Place, ...];
+	AssessmentDeliveryType: [Link, ...];
+	AudienceLevelType: [Link, ...];
+	AudienceType: [Link, ...];
+	AvailabilityListing: [Link, ...];
+	AvailableAt: [Place, ...];
+	AvailableOnlineAt: Link;
+	BroadAlignment: AJAXSettings;
+	CommonConditions: AJAXSettings;
+	CommonCosts: AJAXSettings;
+	ComplaintProcess: [ProcessProfile, ...];
+	CopyrightHolder: AJAXSettings;
+	Corequisite: [ConditionProfile, ...];
+	CredentialId: string;
+	CredentialStatusType: Link;
+	CTID: string;
+	DateEffective: string;
+	DegreeConcentration: [Link, ...];
+	DegreeMajor: [Link, ...];
+	DegreeMinor: [Link, ...];
+	Description: string;
+	DevelopmentProcess: [ProcessProfile, ...];
+	Earnings: [EarningsProfile, ...];
+	EmploymentOutcome: [EmploymentOutcomeProfile, ...];
+	EstimatedCost: [CostProfile, ...];
+	EstimatedDuration: [DurationProfile, ...];
+	ExactAlignment: AJAXSettings;
+	ExpirationDate: string;
+	FinancialAssistance: [FinancialAssistanceProfile, ...];
+	HasPart: AJAXSettings;
+	Holders: [HoldersProfile, ...];
+	Identifier: [IdentifierValue, ...];
+	Image: Link;
+	IndustryType: [Link, ...];
+	// BCP-47 language code for the credential being described, rather than the record itself. Do not confuse this with the RecordLanguage metadata property above.
+	InLanguage: [string, ...];
+	InstructionalProgramType: [Link, ...];
+	IsAdvancedStandingFor: [ConditionProfile, ...];
+	ISICV4: string;
+	IsPartOf: AJAXSettings;
+	IsPreparationFor: [ConditionProfile, ...];
+	IsRecommendedFor: [ConditionProfile, ...];
+	IsRequiredFor: [ConditionProfile, ...];
+	Jurisdiction: [JurisdictionProfile, ...];
+	Keyword: [string, ...];
+	LatestVersion: AJAXSettings;
+	LearningDeliveryType: [Link, ...];
+	MaintenanceProcess: [ProcessProfile, ...];
+	MajorAlignment: AJAXSettings;
+	MinorAlignment: AJAXSettings;
+	NAICS: string;
+	Name: string;
+	NarrowAlignment: AJAXSettings;
+	NextVersion: AJAXSettings;
+	OccupationType: [Link, ...];
+	OfferedBy: AJAXSettings;
+	OfferedIn: [Place, ...];
+	OwnedBy: AJAXSettings;
+	PreparationFrom: [ConditionProfile, ...];
+	PreviousVersion: AJAXSettings;
+	ProcessStandards: Link;
+	ProcessStandardsDescription: string;
+	RecognizedBy: AJAXSettings;
+	RecognizedIn: [Place, ...];
+	Recommends: [ConditionProfile, ...];
+	Region: [Place, ...];
+	RegulatedBy: AJAXSettings;
+	RegulatedIn: [Place, ...];
+	Renewal: [ConditionProfile, ...];
+	RenewalFrequency: [DurationProfile, ...];
+	RenewedBy: AJAXSettings;
+	RenewedIn: [Place, ...];
+	Requires: [ConditionProfile, ...];
+	ReviewProcess: [ProcessProfile, ...];
+	Revocation: [RevocationProfile, ...];
+	RevocationProcess: [ProcessProfile, ...];
+	RevokedBy: AJAXSettings;
+	RevokedIn: [Place, ...];
+	SameAs: AJAXSettings;
+	Subject: [Link, ...];
+	SubjectWebpage: Link;
+	SupersededBy: AJAXSettings;
+	Supersedes: AJAXSettings;
+	TargetPathway: AJAXSettings;
+	VersionIdentifier: [IdentifierValue, ...];
 }
 
 interface Link {
@@ -80,33 +189,34 @@ interface Link {
 }
 
 interface JurisdictionProfile {
-	AssertedBy: {Link};
+	AssertedBy: AJAXSettings;
+	AssertedBy: number;
 	Description: string;
 	GlobalJurisdiction: bool;
-	JurisdictionException: [{Place}, ...];
-	MainJurisdiction: {Place};
+	JurisdictionException: [Place, ...];
+	MainJurisdiction: Place;
 }
 
 interface ProcessProfile {
 	DateEffective: string;
 	Description: string;
-	ExternalInputType: [{Link}, ...];
-	Jurisdiction: [{JurisdictionProfile}, ...];
+	ExternalInputType: [Link, ...];
+	Jurisdiction: [JurisdictionProfile, ...];
 	ProcessFrequency: string;
-	ProcessingAgent: {Link};
-	ProcessMethod: {Link};
+	ProcessingAgent: AJAXSettings;
+	ProcessMethod: Link;
 	ProcessMethodDescription: string;
-	ProcessStandards: {Link};
+	ProcessStandards: Link;
 	ProcessStandardsDescription: string;
-	Region: [{Place}, ...];
+	Region: [Place, ...];
 	ScoringMethodDescription: string;
-	ScoringMethodExample: {Link};
+	ScoringMethodExample: Link;
 	ScoringMethodExampleDescription: string;
-	SubjectWebpage: {Link};
-	TargetAssessment: [{Link}, ...];
-	TargetCompetencyFramework: [{Link}, ...];
-	TargetCredential: [{Link}, ...];
-	TargetLearningOpportunity: [{Link}, ...];
+	SubjectWebpage: Link;
+	TargetAssessment: AJAXSettings;
+	TargetCompetencyFramework: AJAXSettings;
+	TargetCredential: AJAXSettings;
+	TargetLearningOpportunity: AJAXSettings;
 	VerificationMethodDescription: string;
 }
 
@@ -116,139 +226,38 @@ interface IdentifierValue {
 	IdentifierValueCode: string;
 }
 
-interface Credential {
-	// Will always be "Credential"
-	BroadType: string;
-	// Will be one of the subclasses of ceterms:Credential
-	CTDLType: string;
-	// Will always be a BCP-47 language code, forced to all lowercase. Usually "en". This will indicate the language used by all of the relevant string values for this record.
-	RecordLanguage: string;
-	AccreditedBy: [{Link}, ...];
-	AccreditedIn: [{Place}, ...];
-	AdministrationProcess: [{ProcessProfile}, ...];
-	AdvancedStandingFrom: [{Link}, ...];
-	AlternateName: [string, ...];
-	AppealProcess: [{ProcessProfile}, ...];
-	ApprovedBy: [{Link}, ...];
-	ApprovedIn: [{Place}, ...];
-	AssessmentDeliveryType: [{Link}, ...];
-	AudienceLevelType: [{Link}, ...];
-	AudienceType: [{Link}, ...];
-	AvailabilityListing: [{Link}, ...];
-	AvailableAt: [{Place}, ...];
-	AvailableOnlineAt: {Link};
-	BroadAlignment: [{Link}, ...];
-	CommonConditions: [{ConditionManifest}, ...];
-	CommonCosts: [{CostManifest}, ...];
-	ComplaintProcess: [{ProcessProfile}, ...];
-	CopyrightHolder: [{Link}, ...];
-	Corequisite: [{ConditionProfile}, ...];
-	CredentialId: string;
-	CredentialStatusType: {Link};
-	CTID: string;
-	DateEffective: string;
-	DegreeConcentration: [{Link}, ...];
-	DegreeMajor: [{Link}, ...];
-	DegreeMinor: [{Link}, ...];
-	Description: string;
-	DevelopmentProcess: [{ProcessProfile}, ...];
-	Earnings: [{EarningsProfile}, ...];
-	EmploymentOutcome: [{EmploymentOutcomeProfile}, ...];
-	EstimatedCost: [{CostProfile}, ...];
-	EstimatedDuration: [{DurationProfile}, ...];
-	ExactAlignment: [{Link}, ...];
-	ExpirationDate: string;
-	FinancialAssistance: [{FinancialAssistanceProfile}, ...];
-	HasPart: [{Link}, ...];
-	Holders: [{HoldersProfile}, ...];
-	Identifier: [{IdentifierValue}, ...];
-	Image: {Link};
-	IndustryType: [{Link}, ...];
-	// BCP-47 language code for the credential being described, rather than the record itself. Do not confuse this with the RecordLanguage metadata property above.
-	InLanguage: [string, ...];
-	InstructionalProgramType: [{Link}, ...];
-	IsAdvancedStandingFor: [{ConditionProfile}, ...];
-	ISICV4: string;
-	IsPartOf: [{Link}, ...];
-	IsPreparationFor: [{ConditionProfile}, ...];
-	IsRecommendedFor: [{ConditionProfile}, ...];
-	IsRequiredFor: [{ConditionProfile}, ...];
-	Jurisdiction: [{JurisdictionProfile}, ...];
-	Keyword: [string, ...];
-	LatestVersion: [{Link}, ...];
-	LearningDeliveryType: [{Link}, ...];
-	MaintenanceProcess: [{ProcessProfile}, ...];
-	MajorAlignment: [{Link}, ...];
-	MinorAlignment: [{Link}, ...];
-	NAICS: string;
-	Name: string;
-	NarrowAlignment: [{Link}, ...];
-	NextVersion: [{Link}, ...];
-	OccupationType: [{Link}, ...];
-	OfferedBy: [{Link}, ...];
-	OfferedIn: [{Place}, ...];
-	OwnedBy: [{Link}, ...];
-	PreparationFrom: [{ConditionProfile}, ...];
-	PreviousVersion: [{Link}, ...];
-	ProcessStandards: {Link};
-	ProcessStandardsDescription: string;
-	RecognizedBy: [{Link}, ...];
-	RecognizedIn: [{Place}, ...];
-	Recommends: [{ConditionProfile}, ...];
-	Region: [{Place}, ...];
-	RegulatedBy: [{Link}, ...];
-	RegulatedIn: [{Place}, ...];
-	Renewal: [{ConditionProfile}, ...];
-	RenewalFrequency: [{DurationProfile}, ...];
-	RenewedBy: [{Link}, ...];
-	RenewedIn: [{Place}, ...];
-	Requires: [{ConditionProfile}, ...];
-	ReviewProcess: [{ProcessProfile}, ...];
-	Revocation: [{RevocationProfile}, ...];
-	RevocationProcess: [{ProcessProfile}, ...];
-	RevokedBy: [{Link}, ...];
-	RevokedIn: [{Place}, ...];
-	SameAs: [{Link}, ...];
-	Subject: [{Link}, ...];
-	SubjectWebpage: {Link};
-	SupersededBy: [{Link}, ...];
-	Supersedes: [{Link}, ...];
-	TargetPathway: [{Link}, ...];
-	VersionIdentifier: [{IdentifierValue}, ...];
-}
-
 interface ConditionProfile {
-	AlternativeCondition: [{ConditionProfile}, ...];
-	AssertedBy: {Link};
-	AudienceLevelType: [{Link}, ...];
-	AudienceType: [{Link}, ...];
-	CommonCosts: [{CostProfile}, ...];
+	AlternativeCondition: [ConditionProfile, ...];
+	AssertedBy: AJAXSettings;
+	AudienceLevelType: [Link, ...];
+	AudienceType: [Link, ...];
+	CommonCosts: AJAXSettings;
 	Condition: [string, ...];
 	CreditUnitTypeDescription: string;
-	CreditValue: [{ValueProfile}, ...];
+	CreditValue: [ValueProfile, ...];
 	DateEffective: string;
 	Description: string;
-	EstimatedCost: [{CostProfile}, ...];
+	EstimatedCost: [CostProfile, ...];
 	Experience: string;
-	Jurisdiction: [{JurisdictionProfile}, ...];
+	Jurisdiction: [JurisdictionProfile, ...];
 	MinimumAge: number;
 	Name: string;
-	ResidentOf: [{Place}, ...];
-	SubjectWebpage: {Link};
-	SubmissionOf: {Link};
+	ResidentOf: [Place, ...];
+	SubjectWebpage: Link;
+	SubmissionOf: Link;
 	SubmissionOfDescription: string;
-	TargetAssessment: [{Link}, ...];
-	TargetCompetency: [{Link}, ...];
-	TargetCredential: [{Link}, ...];
-	TargetLearningOpportunity: [{Link}, ...];
+	TargetAssessment: AJAXSettings;
+	TargetCompetency: AJAXSettings;
+	TargetCredential: AJAXSettings;
+	TargetLearningOpportunity: AJAXSettings;
 	Weight: number;
 	YearsOfExperience: number;
 }
 
 interface ValueProfile {
-	CreditLevelType: [{Link}, ...];
-	CreditUnitType: [{Link}, ...];
-	Subject: [{Link}, ...];
+	CreditLevelType: [Link, ...];
+	CreditUnitType: [Link, ...];
+	Subject: [Link, ...];
 	Description: string;
 	MaxValue: number;
 	MinValue: number;
@@ -257,27 +266,27 @@ interface ValueProfile {
 }
 
 interface CostProfile {
-	AudienceType: [{Link}, ...];
+	AudienceType: [Link, ...];
 	Condition: [string, ...];
-	CostDetails: {Link};
+	CostDetails: Link;
 	Currency: string;
 	Description: string;
-	DirectCostType: [{Link}, ...];
+	DirectCostType: [Link, ...];
 	EndDate: string;
-	Jurisdiction: [{JurisdictionProfile}, ...];
+	Jurisdiction: [JurisdictionProfile, ...];
 	Name: string;
 	PaymentPattern: string;
 	Price: number;
-	Region: [{Place}, ...];
-	ResidencyType: [{Link}, ...];
+	Region: [Place, ...];
+	ResidencyType: [Link, ...];
 	StartDate: string;
 }
 
 interface DurationProfile {
 	Description: string;
-	ExactDuration: {Duration};
-	MaximumDuration: {Duration};
-	MinimumDuration: {Duration};
+	ExactDuration: Duration;
+	MaximumDuration: Duration;
+	MinimumDuration: Duration;
 }
 
 interface Duration {
@@ -292,10 +301,10 @@ interface Duration {
 
 interface FinancialAssistanceProfile {
 	Description: string;
-	FinancialAssistanceType: [{Link}, ...];
-	FinancialAssistanceValue: [{QuantitativeValue}, ...];
+	FinancialAssistanceType: [Link, ...];
+	FinancialAssistanceValue: [QuantitativeValue, ...];
 	Name: string;
-	SubjectWebpage: {Link};
+	SubjectWebpage: Link;
 }
 
 interface QuantitativeValue {
@@ -303,30 +312,30 @@ interface QuantitativeValue {
 	MaxValue: number;
 	MinValue: number;
 	Percentage: number;
-	UnitText: [{Link}, ...];
+	UnitText: [Link, ...];
 	Value: number;
 }
 
 interface ConditionManifest {
-	ConditionManifestOf: {Link};
-	Corequisite: [{ConditionProfile}, ...];
+	ConditionManifestOf: AJAXSettings;
+	Corequisite: [ConditionProfile, ...];
 	CTID: string;
 	Description: string;
-	EntryCondition: [{ConditionProfile}, ...];
+	EntryCondition: [ConditionProfile, ...];
 	Name: string;
-	Recommends: [{ConditionProfile}, ...];
-	Renewal: [{ConditionProfile}, ...];
-	Requires: [{ConditionProfile}, ...];
-	SubjectWebpage: {Link};
+	Recommends: [ConditionProfile, ...];
+	Renewal: [ConditionProfile, ...];
+	Requires: [ConditionProfile, ...];
+	SubjectWebpage: Link;
 }
 
 interface CostManifest {
-	CostDetails: {Link};
-	CostManifestOf: {Link};
+	CostDetails: Link;
+	CostManifestOf: Link;
 	CTID: string;
 	Description: string;
 	EndDate: string;
-	EstimatedCost: [{CostProfile}, ...];
+	EstimatedCost: [CostProfile, ...];
 	Name: string;
 	StartDate: string;
 }
@@ -334,22 +343,43 @@ interface CostManifest {
 interface EarningsProfile {
 	DateEffective: string;
 	HighEarnings: number;
-	Jurisdiction: [{JurisdictionProfile}, ...];
+	Jurisdiction: [JurisdictionProfile, ...];
 	LowEarnings: number;
 	MedianEarnings: number;
 	Name: string;
 	PostReceiptMonths: number;
-	Region: [{Place}, ...];
-	Source: {Link};
-	RelevantDataSet: [{Link}, ...];
+	Region: [Place, ...];
+	Source: Link;
+	RelevantDataSet: AJAXSettings;
 }
 
 interface EmploymentOutcomeProfile {
 	DateEffective: string;
 	Description: string;
 	JobsObtained: number;
-	Jurisdiction: [{JurisdictionProfile}, ...];
+	Jurisdiction: [JurisdictionProfile, ...];
 	Name: string;
-	Source: {Link};
-	RelevantDataSet: [{Link}, ...];
+	Source: Link;
+	RelevantDataSet: AJAXSettings;
+}
+
+interface HoldersProfile {
+	DateEffective: string;
+	DemographicInformation: string;
+	Description: string;
+	Jurisdiction: [JurisdictionProfile, ...];
+	Name: string;
+	NumberAwarded: number;
+	Region: [Place, ...];
+	Source: Link;
+	RelevantDataSet: AJAXSettings;
+}
+
+interface AJAXSettings {
+	// Label used for display
+	Label: string;
+	// Pre-determined total number of results for this AJAX request. If present and greater than 0, execute the request; otherwise, ignore it.
+	Total: number;
+	// Generic JSON object containing any relevant query data to pass to the server when executing the request.
+	QueryData: Object;
 }
